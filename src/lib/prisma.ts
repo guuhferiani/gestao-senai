a4d1ae2f9cb26e22f1b7fc38bd633df911ac13ab
guuhferiani <guuh.feriani@gmail>
2026-07-29 16:07:06 -0300

    fix: resolve erro do next-auth build

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
 import { Pool } from 'pg'
 
-const connectionString = process.env.DATABASE_URL!
+const connectionString = `${process.env.DATABASE_URL || "postgres://dummy:dummy@localhost/dummy"}`
 
 const pool = new Pool({ connectionString })
 const adapter = new PrismaPg(pool)
