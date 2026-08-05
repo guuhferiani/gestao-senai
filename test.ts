a4c9da23cd9c7c880c59459feaa99d94245e711b
guuhferiani <guuh.feriani@gmail.com>
2026-08-05 20:30:52 -0300

    fix: use PrismaNeonHttp adapter for instant firewall-safe DB connections over HTTPS

--- test.ts
+++ test.ts
@@ -1,7 +1,10 @@
 import 'dotenv/config';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from './src/generated/prisma/client';
+
+import { PrismaNeonHttp } from '@prisma/adapter-neon';
 
 console.log('URL from process.env:', process.env.DATABASE_URL);
 
-const prisma = new PrismaClient();
+const adapter = new PrismaNeonHttp(process.env.DATABASE_URL || '', {});
+const prisma = new PrismaClient({ adapter });
 prisma.usuario.count().then(console.log).catch(console.error);
